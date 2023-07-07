7e038a98003431a763d9bca33985a4a06ba80727
Damicable <annobidiegwu@gmail.com>
2023-07-07 16:54:02 -0700

    Indentation updated

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -6,7 +6,7 @@ This module supplies one function, text_indentation(text).
 
 
 def text_indentation(text):
-    """This function splits a text into lines along "?", ":", "." followed by 2 new lines"""
+    """This is a function that prints a text with 2 new lines after each of these characters: ., ? and :"""
     if type(text) is not str:
         raise TypeError("text must be a string")
     flag = 0
