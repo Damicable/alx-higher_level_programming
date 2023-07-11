fc12e3c95057d5ebf16072e7f32d88b44da78d12
Damicable <annobidiegwu@gmail.com>
2023-07-11 15:00:51 -0700

    Updating python stdin/out

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-This script adds all arguments to a Python list, and then saves them to a file
+Script that adds all arguments to a Python list, and then saves
+them to a file
 """
 
 from sys import argv
