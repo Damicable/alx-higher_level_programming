405580ae0a6b2ef0ca0759dc7b6d0e29393f81e8
Damicable <annobidiegwu@gmail.com>
2023-07-11 14:28:05 -0700

    Python std in/out

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This function contain the JSON string
+"""
+
+import json
+
+
+def to_json_string(my_obj):
+    """This returns the JSON representation of an object (string)"""
+    return json.dumps(my_obj)
