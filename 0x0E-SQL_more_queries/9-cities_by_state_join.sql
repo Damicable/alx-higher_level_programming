cfe95edb3a64bf2394a01d2421a968c3f2f17817
Damicable <annobidiegwu@gmail.com>
2023-08-16 15:40:38 -0700

    My database

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,7 @@
+-- This script lists all cities in the database hbtn_0d_usa.
+-- Records are sorted in an ascending order cities.id.
+SELECT c.`id`, c.`name`, s.`name`
+  FROM `cities` AS c
+       INNER JOIN `states` AS s
+       ON c.`state_id` = s.`id`
+ ORDER BY c.`id`;
