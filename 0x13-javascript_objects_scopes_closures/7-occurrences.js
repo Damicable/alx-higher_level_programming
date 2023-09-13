08493936147548b9f33b76b4e97b7ceafe364073
Damicable <annobidiegwu@gmail.com>
2023-09-13 15:28:27 -0700

    Occurrences

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,13 +1,10 @@
 #!/usr/bin/node
-
 exports.nbOccurences = function (list, searchElement) {
-  let timesOccure = 0;
-
-  for (let i = 0; i < list.length; i++) {
-   if (list[i] === searchElement) {
-     timesOccure++;
-   }
+  let count = 0;
+  for (const i of list) {
+    if (i === searchElement) {
+      count++;
+    }
   }
-
-  return timesOcccure;
+  return count;
 };
