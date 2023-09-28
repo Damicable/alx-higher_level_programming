3066963a745b24b6a5056ea5ad4dbd6ebb7bfb51
Damicable <annobidiegwu@gmail.com>
2023-09-28 16:26:06 -0700

    Python network peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,26 +1,26 @@
 #!/usr/bin/python3
-"""This module defines a peak-finding algorithm."""
+""" This defines a peak-finding algorithm."""
 
 
 def find_peak(list_of_integers):
-    """ This finds the peak in a list of unsorted integer"""
+    """ Finds the peak in a list of an unsorted integers """
     if list_of_integers == []:
         return None
 
     length = len(list_of_integers)
     k = int(length / 2)
-    lis = list_of_integers
+    li = list_of_integers
 
     if k - 1 < 0 and k + 1 >= length:
-        return lis[k]
+        return li[k]
     elif k - 1 < 0:
-        return lis[k] if lis[k] > lis[k + 1] else lis[k + 1]
+        return li[k] if li[k] > li[k + 1] else li[k + 1]
     elif k + 1 >= length:
-        return lis[k] if lis[k] > lis[k - 1] else lis[k - 1]
+        return li[k] if li[k] > li[k - 1] else li[k - 1]
 
-    if lis[k - 1] < lis[k] > lis[k + 1]:
-        return lis[k]
+    if li[k - 1] < li[k] > li[k + 1]:
+        return li[k]
 
-    if lis[k + 1] > lis[k - 1]:
-        return find_peak(lis[k:])
-    return find_peak(lis[:k])
+    if li[k + 1] > li[k - 1]:
+        return find_peak(li[k:])
+    return find_peak(li[:k])
