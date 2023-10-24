7ae2d95fea2be8960c21ef28fce338594e83c725
Damicable <annobidiegwu@gmail.com>
2023-10-24 10:52:13 -0700

    Completed

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
+
 const request = require('request');
-#!/usr/bin/node
-const request = require('request');
+
 request(process.argv[2], function (error, response, body) {
   if (!error) {
     const todos = JSON.parse(body);
@@ -10,9 +10,9 @@ request(process.argv[2], function (error, response, body) {
       if (todo.completed && completed[todo.userId] === undefined) {
         completed[todo.userId] = 1;
       } else if (todo.completed) {
-          completed[todo.userId] += 1;
-        }
-      });
-  console.log(completed);
+        completed[todo.userId] += 1;
+      }
+    });
+    console.log(completed);
   }
 });
